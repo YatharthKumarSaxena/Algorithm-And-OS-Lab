eedcff75c6a1ab62c688f9a305765a092dfb682e
YatharthKumarSaxena <yatharthsaxena25@gmail.com>
2025-10-15 09:07:48 +0530

    Programs of Day-9 Modified

--- Day-9/readerWriter.cpp
+++ Day-9/readerWriter.cpp
@@ -14,33 +14,33 @@ int reader_count = 0;
 
 mt19937 rng;
 
-class Reader {
+class Reader{
 private:
     int id;
 
 public:
-    Reader(int i) : id(i) {}
+    Reader(int i): id(i){}
 
-    void operator()() {
-        while (true) {
+    void operator()(){
+        while(true){
             {
                 lock_guard<mutex> lock(reader_count_mutex);
                 reader_count++;
-                if (reader_count == 1) {
+                if(reader_count == 1){
                     resource_mutex.lock();
                 }
             }
 
             {
                 lock_guard<mutex> lock(io_mutex);
-                cout << "Reader " << id << " is reading data: " << shared_data << " (Total readers: " << reader_count << ")" << endl;
+                cout<<"Reader "<<id<<" is reading data:"<<shared_data<<" (Total readers:"<<reader_count<<")"<<endl;
             }
             this_thread::sleep_for(chrono::milliseconds(rng() % 1500 + 500));
 
             {
                 lock_guard<mutex> lock(reader_count_mutex);
                 reader_count--;
-                if (reader_count == 0) {
+                if(reader_count == 0){
                     resource_mutex.unlock();
                 }
             }
@@ -50,49 +50,45 @@ public:
     }
 };
 
-class Writer {
+class Writer{
 private:
     int id;
 
 public:
-    Writer(int i) : id(i) {}
+    Writer(int i): id(i){}
 
-    void operator()() {
-        while (true) {
+    void operator()(){
+        while(true){
             int new_value;
-            
             {
                 lock_guard<mutex> io_lock(io_mutex);
-                cout << endl << ">>> Writer " << id << ", enter new value: ";
-                cin >> new_value;
-            } 
-
+                cout<<endl<<">>> Writer "<<id<<", enter new value:";
+                cin>>new_value;
+            }
             {
                 lock_guard<mutex> resource_lock(resource_mutex);
                 shared_data = new_value;
-
                 {
                     lock_guard<mutex> io_lock(io_mutex);
-                    cout << ">>> Writer " << id << " wrote data: " << shared_data << " <<<" << endl;
+                    cout<<">>> Writer "<<id<<" wrote data:"<<shared_data<<" <<<"<<endl;
                 }
                 this_thread::sleep_for(chrono::milliseconds(rng() % 800 + 500));
             }
-
             this_thread::sleep_for(chrono::milliseconds(rng() % 1500 + 1000));
         }
     }
 };
 
-int main() {
+int main(){
     int num_readers, num_writers;
-    cout << "Enter the number of readers: ";
-    cin >> num_readers;
-    cout << "Enter the number of writers: ";
-    cin >> num_writers;
+    cout<<"Enter the number of readers:";
+    cin>>num_readers;
+    cout<<"Enter the number of writers:";
+    cin>>num_writers;
 
     rng.seed(chrono::high_resolution_clock::now().time_since_epoch().count());
 
-    cout << "Readers-Writers Simulation Started." << endl;
+    cout<<"Readers-Writers Simulation Started."<<endl;
 
     int total_threads = num_readers + num_writers;
     Reader** readers = new Reader*[num_readers];
@@ -100,24 +96,24 @@ int main() {
     thread* threads = new thread[total_threads];
     int thread_index = 0;
 
-    for (int i = 0; i < num_readers; ++i) {
+    for(int i = 0; i < num_readers; ++i){
         readers[i] = new Reader(i + 1);
         threads[thread_index++] = thread(ref(*readers[i]));
     }
 
-    for (int i = 0; i < num_writers; ++i) {
+    for(int i = 0; i < num_writers; ++i){
         writers[i] = new Writer(i + 1);
         threads[thread_index++] = thread(ref(*writers[i]));
     }
 
-    for (int i = 0; i < total_threads; ++i) {
+    for(int i = 0; i < total_threads; ++i){
         threads[i].join();
     }
 
-    for (int i = 0; i < num_readers; ++i) {
+    for(int i = 0; i < num_readers; ++i){
         delete readers[i];
     }
-    for (int i = 0; i < num_writers; ++i) {
+    for(int i = 0; i < num_writers; ++i){
         delete writers[i];
     }
     delete[] readers;
